b0d70094961739afe0cf8eaed153fd0f9fe0ab7a
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-08-08 21:18:11 -0300

    Finalizando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import EstiloGlobal, { Container } from './styles'
+
+import store from './store'
+import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <Cadastro />
+  }
+])
+
 function App() {
   return (
-    <div className="App">
-      <h1>Olá react</h1>
-    </div>
+    <Provider store={store}>
+      <EstiloGlobal />
+      <Container>
+        <RouterProvider router={rotas} />
+      </Container>
+    </Provider>
   )
 }
 
